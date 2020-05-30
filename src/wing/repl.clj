7e13c4b897f53330cf52e9659bd31ec3c3033547
Ryan Schmukler <rschmukler@gmail.com>
2020-05-29 21:21:56 -0400

    deps: remove git-pinned deps
    Removes git pinned dependencies from wing, getting it to a more
    "library-like" status.
    
    This required implementing something akin to the add-lib branch of
    tools.deps (which is why we dependend on the git repo to begin with),
    hence the new dep on pomegranate

--- src/wing/repl.clj
+++ src/wing/repl.clj
@@ -1,23 +1,47 @@
 (ns wing.repl
   "REPL namespace for Wing"
-  (:require [clojure.tools.deps.alpha.repl :refer [add-lib]]
+  (:require [clojure.tools.deps.alpha :as tools.deps]
+            [cemerick.pomegranate :as pome]
             [wing.integrant :as wing.ig]
             [integrant.core :as ig]
             [integrant.repl :as ig.repl]
             [integrant.repl.state]))
 
-(defn sync-libs
+(defn- deps->class-paths!
+  "Convert a map of deps into a sequence of class paths.
+
+  See `clojure.tools.deps.alpha/resolve-deps` for `arg-maps` documentation."
+  ([deps] (deps->class-paths! deps {}))
+  ([deps arg-map]
+   (->> (tools.deps/resolve-deps
+          {:deps deps
+           :mvn/repos
+           {"central" {:url "https://repo1.maven.org/maven2/"}
+            "clojars" {:url "https://repo.clojars.org/"}}}
+          arg-map)
+        (vals)
+        (mapcat :paths))))
+
+(defn sync-libs!
   "Dynamically refreshes all libs in `deps.edn` and installs any missing.
 
   Optionally takes an alias to look under."
-  ([] (sync-libs nil))
+  ([] (sync-libs! nil))
   ([?alias]
-   (let [edn  (-> "deps.edn" slurp read-string)
-         libs (if ?alias
-                (get-in edn [:aliases ?alias :extra-deps])
-                (get edn :deps))]
-     (doseq [[lib-sym lib-spec] libs]
-       (add-lib lib-sym lib-spec)))))
+   (let [edn        (-> "deps.edn" slurp read-string)
+         deps       (get edn :deps)
+         extra-deps (when ?alias
+                      (get-in edn [:aliases ?alias :extra-deps]))]
+     (->> (deps->class-paths! deps {:extra-deps extra-deps})
+          (run! pome/add-classpath)))))
+
+
+(defn add-lib!
+  "Add a single library to the active classpath."
+  [lib-sym lib-spec]
+  (->> (deps->class-paths! {lib-sym lib-spec})
+       (run! pome/add-classpath)))
+
 
 (defn find-config
   "Returns a running integrant config with the provided key.
